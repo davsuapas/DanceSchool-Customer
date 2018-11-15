49a202428aaf5f9d0d3a56cae7336b4a3cc00bd8
David Suarez <dav.sua.pas@gmail.com>
2018-11-15 13:16:23 +0100

    configure smoke profile

---
+++ src/test/java/smoke/SmokeTest.java
@@ -0,0 +1,13 @@
+package smoke;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class SmokeTest {
+
+    @Test
+    public void checkonly_infrastructures() {
+    }
+}
