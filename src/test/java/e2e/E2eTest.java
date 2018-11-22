9a7107430e5203ffe4730c0484032814b612c6b1
David Suarez <dav.sua.pas@gmail.com>
2018-11-22 16:33:36 +0100

    e2e test

---
+++ src/test/java/e2e/E2eTest.java
@@ -0,0 +1,14 @@
+package e2e;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class E2eTest {
+
+    @Test
+    public void checkonly_infrastructures() {
+    }
+
+}
